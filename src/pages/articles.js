d8bf9a1e2de92eee28f311439c8895669fcc2528
Patryk Orłowski <orzeleagle122@gmail.com>
2021-04-29 16:18:21 +0200

    commit message

--- src/pages/articles.js
+++ src/pages/articles.js
@@ -1,34 +1,40 @@
-import React from 'react';
-import styled from 'styled-components';
-import { graphql } from 'gatsby';
-import ArticlePreview from '../components/ArticlePreview/ArticlePreview';
-import PageInfo from '../components/PageInfo/PageInfo';
+import React from "react"
+import styled from "styled-components"
+import { graphql } from "gatsby"
+import ArticlePreview from "../components/ArticlePreview/ArticlePreview"
+import PageInfo from "../components/PageInfo/PageInfo"
 
 const ArticlesWrapper = styled.div`
   display: grid;
   grid-template-columns: repeat(3, 1fr);
   grid-gap: 50px;
-`;
+`
 
 const pageData = {
-  title: 'articles',
+  title: "articles",
   paragraph: `While artists work from real to the abstract, architects must work from the abstract to the real.`,
 }
 
 const ArticlesPage = ({ data }) => {
-  const { allMdx: { nodes }} = data;
+  // const { allMdx: { nodes }} = data;
+  const { nodes } = data.allMdx
   return (
     <>
       <PageInfo title={pageData.title} paragraph={pageData.paragraph} />
       <ArticlesWrapper>
-      {nodes.map(({excerpt, frontmatter: {title, slug, featuredImage}}) => (
-        <ArticlePreview title={title} excerpt={excerpt}  background={featuredImage.childImageSharp.fluid.src}/>
-      ))}
+        {nodes.map(
+          ({ excerpt, frontmatter: { title, slug, featuredImage } }) => (
+            <ArticlePreview
+              title={title}
+              excerpt={excerpt}
+              background={featuredImage.childImageSharp.fluid.src}
+            />
+          )
+        )}
       </ArticlesWrapper>
     </>
-  );
+  )
 }
-
 
 export const query = graphql`
   {
@@ -49,6 +55,6 @@ export const query = graphql`
       }
     }
   }
-`;
+`
 
-export default ArticlesPage;
+export default ArticlesPage
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,50 +1,50 @@
-import React from "react";
-import Button from "../components/Button/Button";
-import { graphql } from "gatsby";
-import styled from 'styled-components';
-import Image from 'gatsby-image';
-import Post from "../components/Post/Post";
+import React from "react"
+import Button from "../components/Button/Button"
+import { graphql } from "gatsby"
+import styled from "styled-components"
+import Image from "gatsby-image"
+import Post from "../components/Post/Post"
 
-const ContentWrapper=styled.div`
-  width:65%;
-  height:calc(100vh - 80px);
-  text-align:right;
-  display:flex;
-  flex-direction:column;
-  justify-content:center;
-  align-items:flex-end;
+const ContentWrapper = styled.div`
+  width: 65%;
+  height: calc(100vh - 80px);
+  text-align: right;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-end;
 
-  h1{
-    font-size:85px;
-    margin:0;
-    width:40%;
-    line-height:0.9;
+  h1 {
+    font-size: 85px;
+    margin: 0;
+    width: 40%;
+    line-height: 0.9;
   }
 
-  p{
-    margin:20px 0 40px;
-    width:40%;
+  p {
+    margin: 20px 0 40px;
+    width: 40%;
   }
+`
 
-`;
+const ImageWrapper = styled(Image)`
+  position: absolute !important;
+  top: 0;
+  right: 0;
+  width: 35%;
+  object-fit: cover;
+  height: 100vh;
+`
 
-const ImageWrapper=styled(Image)`
-  position:absolute !important;
-  top:0;
-  right:0;
-  width:35%;
-  object-fit:cover;
-  height:100vh;
-`;
-
-
-const IndexPage = ({data}) => (
+const IndexPage = ({ data }) => (
   <>
     <ContentWrapper>
-      <Post/>
+      <Post />
       <h1>Your new space</h1>
       <p>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque a magna diam. Praesent eget dolor laoreet turpis lacinia volutpat a et turpis. Donec urna ante, varius sit amet arcu in, eleifend sagittis est.
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque a magna
+        diam. Praesent eget dolor laoreet turpis lacinia volutpat a et turpis.
+        Donec urna ante, varius sit amet arcu in, eleifend sagittis est.
       </p>
       <Button>estimete project</Button>
     </ContentWrapper>
@@ -54,16 +54,16 @@ const IndexPage = ({data}) => (
 )
 
 export const query = graphql`
-{
-  file(name: {eq: "hero"}) {
-    publicURL
-    childImageSharp {
-      fluid(maxWidth: 800, maxHeight: 1200, quality: 90) {
-        ...GatsbyImageSharpFluid_tracedSVG
+  {
+    file(name: { eq: "hero" }) {
+      publicURL
+      childImageSharp {
+        fluid(maxWidth: 800, maxHeight: 1200, quality: 100) {
+          ...GatsbyImageSharpFluid_tracedSVG
+        }
       }
     }
   }
-}
 `
 
-export default IndexPage;
+export default IndexPage
